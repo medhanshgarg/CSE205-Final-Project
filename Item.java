84ba98de0f1178fa2c9fa2386a595e08db55a6db
jamesweber7 <72582898+jamesweber7@users.noreply.github.com>
2021-04-25 13:05:36 -0700

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,18 +1,26 @@
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
 
 public class Item {
 
 	private String name;
 	private int id;
 	private double price;
+	private ArrayList<String> tags;
 	
 	Item() {
-		this("UNTITLED", -1, -1);
+		this("UNTITLED", -1, -1, new ArrayList<String>());
 	}
 	
-	Item(String name, int id, double price) {
-		this.name = name;
+	Item(String name, int id, double price, ArrayList<String> tags) {
+		this.name = name.toUpperCase();
 		this.id = id;
 		this.price = price;
+		this.tags = tags;
+		if (this.tags.indexOf(this.name) == -1)
+			this.tags.add(this.name);
 	}
 	
 	public String getName() {
@@ -20,7 +28,9 @@ public class Item {
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		tags.remove(tags.indexOf(this.name));
+		this.name = name.toUpperCase();
+		tags.add(this.name);
 	}
 
 	public int getId() {
@@ -34,19 +44,46 @@ public class Item {
 	public double getPrice() {
 		return price;
 	}
+	
+	public String getPriceCurrencyFormat() {
+		NumberFormat formatter = NumberFormat.getCurrencyInstance();
+		String price = formatter.format(getPrice());
+		return price;
+	}
 
 	public void setPrice(double price) {
 		this.price = price;
 	}
 	
+	public ArrayList<String> getTags() {
+		return this.tags;
+	}
+	
+	public void setTags(ArrayList<String> tags) {
+		this.tags = tags;
+	}
+	
+	// MAYBE REMOVE
+	public JLabel itemLabel(int x, int y, int width, int height) {
+		JLabel itemLabel = new JLabel();
+		itemLabel.setText(getName());
+		itemLabel.setBounds(x,y,width,height);
+		return itemLabel;
+	}
+	
 	public String toString() {
 		
-		String str = "Item #";
-		str += getId() + ": ";
-		str += getName() + ", $";
-		str += getPrice();
+		String str = getName();
+		str += " (" + getId() + ")";
 		return str;
 		
+	}
+	
+	public String allToString() {
+		String str = toString() + ", ";
+		str += getPrice() + ", ";
+		str += getTags().toString();
+		return str;
 	}
 	
 }
